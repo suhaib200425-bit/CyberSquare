befaf8bf2c31e4619adfc314288844879a2a9814
Suhaib2006 <suhaibkoppath66@gmail.com>
2026-04-04 21:58:38 +0530

    profile change one

--- Portfolio/src/sections/About/About.jsx
+++ Portfolio/src/sections/About/About.jsx
@@ -4,30 +4,7 @@ import { ProfileImage } from '../../assets/assets'
 function About() {
     return (
         <div className='About'>
-            <div className="Content row">
-                <div className="col-12 col-md-5 About-image">
-                    <img src={ProfileImage} alt="" srcset="" />
-                </div>
-                <div className="About-image-gradient1"></div>
-                <div className="About-image-gradient2"></div>
-                <div className="col-12 col-md-7 About-Info">
-                    <h2>ABOUT ME</h2>
-                    <p>
-                       Hi, I’m Suhaib, a MERN Stack Developer. I am interested in building full-stack web applications using React, Node.js, Express, and MongoDB.
-
-                    </p>
-                    <p>
-                        I am always ready to learn new technologies and develop real-world projects. I focus on building clean user interfaces and efficient backends.
-
-                    </p>
-                    <p>
-                        My goal is to grow into a professional full-stack developer in the future.
-
-                    </p>
-                <hr style={{height:'5px',borderRadius:'10px',backgroundColor:'white'}} />
-
-                </div>
-            </div>
+            
         </div>
     )
 }
--- Portfolio/src/sections/Indro.jsx
+++ Portfolio/src/sections/Indro.jsx
@@ -2,17 +2,32 @@ import React from 'react'
 import './Indro.css'
 import NavBar from '../components/NavBar/NavBar'
 import { ProfileImage } from '../assets/assets'
+import InfoCard from '../components/InfoCard/InfoCard'
 function Indro() {
     return (
-        <div className='Indro'>
-            <div className="ProfileImage">
-                <img src={"https://i.pinimg.com/736x/62/16/9f/62169fb4f961f71ff689f1d6a215dc6f.jpg"} alt="" />
+        <>
+            <div className='Indro'>
+                <div className="ProfileImage">
+                    <img src={"https://i.pinimg.com/736x/62/16/9f/62169fb4f961f71ff689f1d6a215dc6f.jpg"} alt="" />
+                </div>
+                <div className="ProfileInfo">
+                    <h1>SUHAIB-K</h1>
+                    <p>Web Develeper</p>
+                </div>
+
             </div>
-            <div className="ProfileInfo">
-                <h1>SUHAIB-K</h1>
-                <p>Web Develeper</p>
+            <div className="row OtherContent">
+                <div className="col-12 col-md-4">
+                    <InfoCard title={'Deeper Skillset'} />
+                </div>
+                <div className="col-12 col-md-4">
+                    <InfoCard title={'Creative Work'} />
+                </div>
+                <div className="col-12 col-md-4">
+                    <InfoCard title={'Strong Dedication'} />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
