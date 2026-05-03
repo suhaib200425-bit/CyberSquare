0ce2c724ebd74f38e9c7ed460279ef87b8fe3f4d
Suhaib2006 <suhaibkoppath66@gmail.com>
2026-05-03 08:04:10 +0530

    lm

--- CMS(ContentManegmentSystem)/myApp/src/assets/assets.js
+++ CMS(ContentManegmentSystem)/myApp/src/assets/assets.js
@@ -1,9 +1,9 @@
 import {QueryClient} from '@tanstack/react-query'
 export const queryClient = new QueryClient()
 
-// export const BASEURL ='http://172.23.112.119:5000'
+export const BASEURL ='http://172.23.112.119:5000'
 // export const BASEURL='http://localhost:5000'
-export const BASEURL ='https://thee-slightly-dowry.ngrok-free.dev'
+// export const BASEURL ='https://thee-slightly-dowry.ngrok-free.dev'
 
 export const PAGEAPI=`${BASEURL}/api/page`
 export const CATEGORYAPI=`${BASEURL}/api/category`
